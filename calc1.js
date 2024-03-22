272f2a78ac6c09cd65a71b1338f5f211a440396c
Ubiratan <ubiratanjr99@gmail.com>
2024-03-22 14:16:54 -0300

    commit 2 - atualização

--- calc1.js
+++ calc1.js
@@ -1,12 +1,17 @@
 function calc() {
+    //Variáveis que vão salvar os elementos dentro delas para serem manipulados posteriormente.
     var p1 = document.getElementById('i_peso').value
     var a1 = document.getElementById('i_alt').value
+    var t1 = document.getElementById('text_bloco')
+    //Conversão dos valores digitados pelo usuário nos elementos citado acima em número.
     var peso = Number(p1)
     var altura = Number(a1)
+    //Variável que vai fazer o cálculo do IMC.
     var calc_imc = p1 / (a1 ** 2)
+    //Conversão do valor do cálculo para número.
     var result = Number(calc_imc)
-    var t1 = document.getElementById('text_bloco')
 
+    //Estrutura condicional para definir a faixa de IMC que a pessoa se encaixa e mostrar uma mensagem personalizada na tela.
     if (result < 18.5) {
         t1.innerHTML = `Seu IMC: ${result.toFixed(2)} <br>Você está abaixo do peso.`
     } else if (result > 18.5 && result <= 24.9) {
